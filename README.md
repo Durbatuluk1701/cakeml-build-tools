91458b7055a4e635289d1906e61364660d1553d1
Will Thomas <30wthomas@gmail.com>
2025-03-29 16:23:04 -0500

    Basic first implementation of the tool

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# cakeml-build-tools
-Tools for building CakeML project
+# CakeML Build Tools
+
+This is a tool I created to help build CakeML projects.
+In particular, I plan to make this compatible with [EasyBakeCakeML](https://github.com/Durbatuluk1701/EasyBakeCakeML) so that you can have Rocq/Coq code extracted to "dependency-annotated" `.cml` files, and then use this tool to further progress your build process.
+
+Make sure to review [quirks](#quirks) for some oddities about the tool that you might trip up on.
+
+### The Overall Idea
+
+CakeML does not have module/library/file imports, which can be rather painful when dealing with a large project.
+
+My solution to this add a sort of "meta" import language that fits into CakeML comments. The standard is basically as follows:
+
+Each .cml file that depends on another file will have its **first** line annotated with a comment
+
+```sml
+(* deps: <dependencies>+ *)
+```
+
+Some special processing is done for `dependencies`:
+
+- If you depend on a file `X.cml` you just have to denote it with `X` and do not need to include the file extension
+- You can specify dependencies nested in a folder via either `Y/X` or `Y.X`, these are considered equivalent by the resolver.
+- Any dependency that does not start with `@` will be presumed to be relatively sourced, while `@` will be 'project-root' sourced.
+
+  For example:
+
+  - If you have dependency `X` in file `Y/Z.cml`, it is assumed that the dependency is at `Y/X.cml`.
+  - This is compared to a dependency `@X` in `Y/Z.cml`, it is assumed that the dependency is `X.cml`
+
+## Modes
+
+There are a couple different options for how to use this tool. They are primarly controlled by the `--mode` argument
+
+### `--mode print`
+
+In print mode, all we do is resolve the dependencies and print them out. The default is to print them to the command line, but you can also print them to a file via the `--out <file>` command
+
+### `--mode merge`
+
+In merge mode, we resolve the dependencies, then merge them into one monolithic CakeML file (specified by `--out <file>`)
+
+### `-mode build`
+
+In build mode, we first create the monolithic CakeML file, then actually run it through the CakeML compiler `cake`, and finally through a C compiler to create an executable.
+
+## Quirks
+
+This is not an exceptionally well thought out or advanced tool. Their are currently some quirks about how it operates and simplifying assumptions are made.
+If you have options you wish would be added, feel free to PR
+
+#### Quirks:
+
+1. Make sure you read [the overall idea](#the-overall-idea) and [modes](#modes) to understand how the processing is really done
+1. We assume that your C compiler is `gcc`, the basis file is `basis_ffi.c` (and that it is available in the directory the tool is run in) and that the C compiler flags are `-O2 -lm`
+1. Their is no cleaning capabilities yet, sorry
+1. The `--out` argument is for the file you want the **monolithic CakeML file** to be in, we force the executable name in `build` mode to be the `out` file name - its extension
--- gen_deps.py
+++ gen_deps.py
@@ -1,13 +1,6 @@
-"""
-Overall steps:
-1. we take in a file for "main" file
-2. read the file and get its imports
-3. for each import, we check if it is a local file or a library
-"""
-
 import argparse
-import sys
 import os
+import subprocess
 
 
 def validate_file(file_path: str) -> str:
@@ -19,20 +12,29 @@ def validate_file(file_path: str) -> str:
     return file_path
 
 
-def post_proc_module(module: str) -> str:
-    return module.replace(".", "/") + ".cml"
+def post_proc_module(dir: str, module: str) -> str:
+    ROOT_PATTERN = "@"
+    if module.startswith(ROOT_PATTERN):
+        # This is a qualified module name
+        # so dont add the "dir" before it
+        return module[len(ROOT_PATTERN) :].replace(".", "/") + ".cml"
+    return os.path.join(dir, module.replace(".", "/")) + ".cml"
 
 
-def get_modules(file_path: str) -> list[str]:
-    """Read the file and extract 'open' statements."""
+def get_direct_deps(file_path: str) -> list[str]:
     # NOTE: We only allow imports on the first line for now
     try:
         with open(file_path, "r") as file:
+            # Gather file meta-data
+            dir = os.path.dirname(file_path)
+            # get the deps out
             first_line = file.readline().strip()
-            if first_line.startswith("(* open ") and first_line.endswith(" *)"):
-                first_line = first_line[8:-3].strip()
-                modules = first_line.split(" ")
-                return list(map(post_proc_module, modules))
+            PRE_LINE = "(* deps: "
+            POST_LINE = " *)"
+            if first_line.startswith(PRE_LINE) and first_line.endswith(POST_LINE):
+                module_snip = first_line[len(PRE_LINE) : -(len(POST_LINE))].strip()
+                modules = module_snip.split(" ")
+                return list(map(lambda mod: post_proc_module(dir, mod), modules))
             else:
                 # No 'open' statement found
                 return []
@@ -40,31 +42,102 @@ def get_modules(file_path: str) -> list[str]:
         raise ValueError(f"File '{file_path}' does not exist")
 
 
+def get_trans_deps(file_path: str, seen: list[str]) -> list[str]:
+    deps = get_direct_deps(file_path)
+    full_deps = []
+    for dep in deps:
+        if dep not in seen:
+            new_deps = get_trans_deps(dep, seen + [dep] + full_deps)
+            full_deps.extend(new_deps)
+    return full_deps + [file_path]
+
+
+def print_mode(args, deps):
+    if args.out:
+        with open(args.out, "w") as outfile:
+            outfile.write("\n".join(deps))
+    else:
+        print(deps)
+
+
+def merge_mode(args, deps):
+    if not args.out:
+        raise ValueError("Output file name (--out <file>) is required in 'build' mode.")
+    # We presume if someone specifies an output file, they are fine with it being written over
+    # Build a monolithic CakeML file
+    with open(args.out, "w") as outfile:
+        for dep in deps:
+            with open(dep, "r") as infile:
+                outfile.write(infile.read())
+                outfile.write("\n\n")
+    return args.out
+
+
+def build_mode(args, deps):
+    CC = "gcc"
+    BASIS_FILE = "basis_ffi.c"
+    CC_FLAGS = "-O2 -lm"
+    # first we build the monolithic file via merge_mode
+    out_file = merge_mode(args, deps)
+    # then we build the binary
+
+    # Generate assembly file from the monolithic CakeML file
+    asm_file = out_file.replace(".cml", ".S")
+    binary_file = out_file.replace(".cml", "")
+    try:
+        subprocess.run(f"cake < {out_file} > {asm_file}", shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Error during CakeML compilation: {e}")
+
+    # Compile the assembly file into a binary using GCC
+    try:
+        subprocess.run(
+            f"{CC} {BASIS_FILE} {asm_file} {CC_FLAGS} -o {binary_file}",
+            shell=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Error during GCC compilation: {e}")
+
+    print(f"Binary created: {binary_file}")
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Generate dependencies for a CakeML project."
     )
     parser.add_argument(
-        "--main",
+        "main",
         type=validate_file,
-        required=True,
         help="Main CakeML file with the final 'main' function in it.",
+    )
+    parser.add_argument(
+        "--mode",
+        type=str,
+        choices=["print", "merge", "build"],
+        required=True,
+        default="print",
+        help="Mode of operation: 'print' to print dependencies, 'merge' to build a monolithic CakeML file for the project, and 'build' to build a monolithic CakeML file and then create a binary out of it.",
+    )
+    parser.add_argument(
+        "--out",
+        type=str,
+        help="Output file name for the monolithic CakeML file.",
     )
 
     args = parser.parse_args()
 
-    proc_list: list[str] = [args.main]
-    deps = {}
-    while proc_list:
-        current_file = proc_list.pop(0)
-        modules = get_modules(current_file)
-        deps[current_file] = modules
-        for module in modules:
-            if module not in deps.keys() and module not in proc_list:
-                proc_list.append(module)
-    # Print the dependencies
-    for file, deps_list in deps.items():
-        print(f"{file}: {', '.join(deps_list)}")
+    deps = get_trans_deps(args.main, [])
+
+    match args.mode:
+        case "print":
+            print_mode(args, deps)
+        case "merge":
+            merge_mode(args, deps)
+        case "build":
+            build_mode(args, deps)
+        case _:
+            raise ValueError(f"Unknown mode: {args.mode}")
 
 
 if __name__ == "__main__":
